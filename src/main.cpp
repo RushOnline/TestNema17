e998849c8b55a2ae0c5057daddf207e098cf0e15
Евгений Непомнящий <rush.zlo@gmail.com>
2021-11-23 23:11:56 +0300

    simplified for TMC22xx

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
-#define EN_PIN           12 // Enable
-#define DIR_PIN          10 // Direction
-#define STEP_PIN         11 // Step
+#define EN_PIN           11 // Enable
+#define DIR_PIN          12 // Direction
+#define STEP_PIN         13 // Step
 
 AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
 
-const int steps_per_mm = 80;
+const int ms = 16;
 
 void setup() {
   Serial.begin(115200);
 
   Serial.println("* Connect Nema17 motor, it will turn left, then turn right in loop.");
 
-  stepper.setMaxSpeed(50*steps_per_mm);
-  stepper.setAcceleration(1000*steps_per_mm);
+  //stepper.setMinPulseWidth(10);
+  stepper.setMaxSpeed(16000);
+  stepper.setAcceleration(32000);
   stepper.setEnablePin(EN_PIN);
   stepper.setPinsInverted(false, false, true);
   stepper.enableOutputs();
@@ -25,11 +26,9 @@ int dir = 1;
 
 void loop() {
     if (stepper.distanceToGo() == 0) {
-        stepper.disableOutputs();
         delay(100);
-        stepper.move(dir*100*steps_per_mm); // Move 100mm
-        dir = dir * -1; 
-        stepper.enableOutputs();
+        stepper.move(dir*1000*ms);
+        dir = -dir; 
     }
     stepper.run();
 }
